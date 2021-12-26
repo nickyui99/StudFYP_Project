ecc8d34efeea6faa72b2230f4ed0cca468ce0cf4
nickyui99 <93330502+nickyui99@users.noreply.github.com>
2021-12-26 14:43:21 +0800

    Updated by Nicholas

--- html/module5/DAO/StudentDataService.php
+++ html/module5/DAO/StudentDataService.php
@@ -23,16 +23,54 @@
 
                     $lecturer_evaluator = new LecturerEvaluator();
                     $lecturer_evaluator->setEvaluatorID($row['assigned_lect_id']);
+                    $lecturer_evaluator -> setLecturerID($row['lect_id']);
                     $lecturer_evaluator -> setEvaluatorName($row['evaluator_name']);
+                    
+                    //Add to array
                     $lect_evaluator_array[$i] = $lecturer_evaluator;
                     $i++;
                 }
 
-                foreach($lect_evaluator_array as $evaluator){
-                    echo $evaluator->getEvaluatorID();
-                    echo $evaluator->getEvaluatorName();
+                $connection->close();
+
+                foreach($lect_evaluator_array as $i){
+                    $sds = new StudentDataService();
+                    $evaluator = $sds -> getLectEvaluatorDetails($i);
+                    echo "<tr>";
+                    echo "<td>" . $evaluator->getEvaluatorID() . "</td>";
+                    echo "<td>Lecturer</td>";
+                    echo "<td>" . $evaluator->getEvaluatorName() . "</td>";
+                    echo "<td>" . $evaluator->getContactNum() . "</td>";
+                    echo "<td>" . $evaluator->getEmail() . "</td>";
+                    echo "<td> - </td>";
+                    echo "</tr>";
                 }
             }
+        }
+
+        function getLectEvaluatorDetails($lect_evaluator){
+            $db = new Database();
+            
+            $sql_query = "SELECT * FROM lecturer WHERE lect_id = '" . $lect_evaluator->getLecturerID() . "'"; 
+
+            $connection = $db->getConnection();
+
+            // Check connection
+            if ($connection->connect_error) {
+                die("Connection failed: " . $connection->connect_error);
+            }
+
+            $result = $connection->query($sql_query);
+
+            if($result->num_rows > 0){
+                while($row = $result->fetch_assoc()){
+                    $lect_evaluator->setContactNum($row['lect_contact_num']);
+                    $lect_evaluator->setEmail($row['lect_email']);
+                }
+            }
+            
+            $connection->close();
+            return $lect_evaluator;
         }
 
     }
