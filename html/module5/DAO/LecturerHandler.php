3856083ee89775f23985b97b13ce9e5dfb51aa37
nickyui99 <93330502+nickyui99@users.noreply.github.com>
2021-12-31 17:33:00 +0800

    Updated by Nicholas

--- html/module5/DAO/LecturerHandler.php
+++ html/module5/DAO/LecturerHandler.php
@@ -6,4 +6,5 @@ if(isset($_POST['search_assigned_evaluation']) && isset($_POST['lecturer_id'])) 
     $lds = new LecturerDataService();
     $output = $lds->getAssignedEvaluation($_POST['search_assigned_evaluation'], $_POST['lecturer_id']);
     echo $output;
+    
 }
