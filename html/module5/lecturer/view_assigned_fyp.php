37c9e29e36d59b4b3b1eb72efc0b7f1b404716bf
nickyui99 <93330502+nickyui99@users.noreply.github.com>
2022-01-22 15:13:31 +0800

    Updated by Nicholas

--- html/module5/lecturer/view_assigned_fyp.php
+++ html/module5/lecturer/view_assigned_fyp.php
@@ -4,7 +4,7 @@
 <!-- This html template is only for StudFYP lecturer only -->
 
 <?php
-include $_SERVER["DOCUMENT_ROOT"] . '/html/module5/Controller/LecturerHandler.php';
+require_once $_SERVER["DOCUMENT_ROOT"] . '/html/module5/Controller/LecturerHandler.php';
 session_start();
 ?>
 
