a0f89667b823defc5efdf905c2e78be61de4ecca
nickyui99 <93330502+nickyui99@users.noreply.github.com>
2022-01-23 20:39:33 +0800

    test

--- html/module5/Controller/EvaluateFormHandler.php
+++ html/module5/Controller/EvaluateFormHandler.php
@@ -57,12 +57,12 @@ if (isset($_SESSION['lect_id'])) {
 
 else if (isset($_SESSION['ip_id'])) {
     if (isset($_POST['inputProjId']) && isset($_POST['inputStudId']) && isset($_POST['inputFypStage']) && isset($_POST['inputProjTitle']) && isset($_POST['total_mark']) && isset($_POST['submission']) && isset($_POST['inputProjFeedback'])) {
-        $eds = new ExternalDataService();
+        echo "ok";
+        // $eds = new ExternalDataService();
 
-
-        //Get assigned lecturer id
-        $assigned_id = $eds->getAssignedIpId($_SESSION['ip_id']);
-        echo $assigned_id;
+        // //Get assigned lecturer id
+        // $assigned_id = $eds->getAssignedIpId($_SESSION['ip_id']);
+        // echo $assigned_id;
 
         // $ev_rubric_array = $lds->getEvaluationRubric($_POST['submission'], $_POST['inputFypStage']);
 
