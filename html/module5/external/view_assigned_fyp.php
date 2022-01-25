cb0439960198d38aef14e374077ab46c34309ce6
nickyui99 <93330502+nickyui99@users.noreply.github.com>
2022-01-25 21:42:20 +0800

    Remove profile

--- html/module5/external/view_assigned_fyp.php
+++ html/module5/external/view_assigned_fyp.php
@@ -68,12 +68,6 @@ session_start();
             <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="fa fa-user fa-fw"></i> Account</a>
                 <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                    <li>
-                        <a class="dropdown-item" href="#!">My profile</a>
-                    </li>
-                    <li>
-                        <hr class="dropdown-divider" />
-                    </li>
                     <li><a class="dropdown-item" href="controller/logout_handler.php">Logout</a></li>
                 </ul>
             </li>
