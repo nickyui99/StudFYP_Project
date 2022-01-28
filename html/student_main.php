69aca41ed38ce25aa1bedb3d9eb7461a569d5f57
nickyui99 <93330502+nickyui99@users.noreply.github.com>
2022-01-28 12:49:42 +0800

    Update sidebar

--- html/student_main.php
+++ html/student_main.php
@@ -142,11 +142,6 @@ session_start();
                                         <i class="fa fa-circle-thin" aria-hidden="true"></i>
                                     </div>Delete logbook
                                 </a>
-                                <a class="nav-link" href="#">
-                                    <div class="sb-nav-link-icon">
-                                        <i class="fa fa-circle-thin" aria-hidden="true"></i>
-                                    </div>My FYP project
-                                </a>
                             </nav>
                         </div>
 
@@ -191,11 +186,6 @@ session_start();
                                     <div class="sb-nav-link-icon">
                                         <i class="fa fa-circle-thin" aria-hidden="true"></i>
                                     </div>View my evaluator
-                                </a>
-                                <a class="nav-link" href="module5/students/my_evaluation_result.php">
-                                    <div class="sb-nav-link-icon">
-                                        <i class="fa fa-circle-thin" aria-hidden="true"></i>
-                                    </div>My evaluation result
                                 </a>
                             </nav>
                         </div>
