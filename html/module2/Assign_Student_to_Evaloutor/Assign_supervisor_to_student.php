d4d60f36553400c0b41aff89202057c8e02d9cad
aymn101 <63733160+aymn101@users.noreply.github.com>
2022-01-30 07:40:16 +0800

    test by Aiman

--- html/module2/Assign_Student_to_Evaloutor/Assign_supervisor_to_student.php
+++ html/module2/Assign_Student_to_Evaloutor/Assign_supervisor_to_student.php
@@ -13,8 +13,10 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
   <?php
+  include_once '../../../mySQLi/config.php';
   include $_SERVER["DOCUMENT_ROOT"] . '/StudFYP_Project/html/controller/AnnouncementHandler.php';
   include $_SERVER["DOCUMENT_ROOT"] . '/StudFYP_Project/html/controller/FypActivityHandler.php';
+
 
   session_start();
   ?>
